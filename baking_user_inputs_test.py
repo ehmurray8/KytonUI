c7f8369d0ca6895f53d9eeb2175480b4aa9c3b4c
ehmurray8 <ehmurray8@gmail.com>
2017-07-13 01:49:10 -0400

    Added graph.

--- baking_user_inputs_test.py
+++ baking_user_inputs_test.py
@@ -6,11 +6,19 @@ import time
 #import threading
 #import xlsxwriter
 
-import ui_helper
-import controller_340_wrapper as temp_controller
-import init_instruments as init
-import sm125_wrapper
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+#import controller_340_wrapper as temp_controller
+#import init_instruments as init
+#import sm125_wrapper
 import create_options_panel as options_panel
+import ui_helper
+
+from numpy import arange, sin, pi
+
+matplotlib.use('TkAgg')
 
 
 class Application(tk.Frame):
@@ -19,7 +27,7 @@ class Application(tk.Frame):
         """Constructs the app."""
         super().__init__(master)
 
-        self.controller, self.oven, self.gp700, self.sm125 = init.setup_instruments()
+        #self.controller, self.oven, self.gp700, self.sm125 = init.setup_instruments()
 
         #Init member vars
         self.sm125_state = tk.IntVar()
@@ -43,21 +51,40 @@ class Application(tk.Frame):
         self.menu = tk.Menu(master, tearoff=0)
         master.config(menu=self.menu)
         self.pack(side="top", fill="both", expand=True)
-        options_panel.create_options_grid(self, master)
-        ui_helper.create_fibers_sn_grid(self)
-        self.create_start_btn()
 
-    def create_start_btn(self):
-        """Creates the start button in the app."""
-        start_button = tk.Button(self)
-        start_button["text"] = "Start"
-        start_button["command"] = self.start
-        start_button.pack()
+        self.main_frame = tk.Frame(self)
+
+        self.options = options_panel.OptionsPanel(self.main_frame)
+        self.options.create_start_btn(self.start)
+        self.options.grid(row=0, column=0, sticky='ew')
+
+        self.create_graph()
+        self.main_frame.pack(expand=1, fill=tk.BOTH)
+
+
+    def create_graph(self):
+        """Creates the graph."""
+        fig = Figure(figsize=(5, 4), dpi=100)
+        sub = fig.add_subplot(111)
+        rng = arange(0.0, 3.0, 0.01)
+        y_vals = sin(2*pi*rng)
+
+        sub.plot(rng, y_vals)
+
+
+        # a tk.DrawingArea
+        canvas = FigureCanvasTkAgg(fig, self.main_frame)
+        canvas.show()
+        canvas.get_tk_widget().grid(row=0, column=1)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, ROOT)
+        toolbar.update()
 
 
     def start(self):
         """Starts the recording process."""
-        self.baking_loop()
+        #self.baking_loop()
+        ui_helper.print_options(self.options)
         #stable = False
 
         #timer = threading.Timer(int(self.sec_time_entry.get()), self.baking_loop)
@@ -71,76 +98,78 @@ class Application(tk.Frame):
 
         #threading.Timer(int(self.prim_time_entry.get()) * 60,  self.baking_loop()).start()
 
-    def baking_loop(self):
-        """Runs the baking process."""
-        temperature = temp_controller.get_temp_c(self.controller)
-        temperature = float(temperature[:-3])
 
-        amplitudes_avg = []
-        wavelengths_avg = []
-        count = 0
+#    def baking_loop(self):
+#        #Runs the baking process.
+#        temperature = temp_controller.get_temp_c(self.controller)
+#        temperature = float(temperature[:-3])
+#
+#        amplitudes_avg = []
+#        wavelengths_avg = []
+#        count = 0
+#
+#        need_init = False
+#
+#        while count < int(self.num_pts.get()):
+#            wavelengths, amplitudes = sm125_wrapper.get_data_actual(self.sm125)
+#
+#            if not need_init:
+#                wavelengths_avg = [0] * len(wavelengths[0])
+#                amplitudes_avg = [0] * len(amplitudes[0])
+#                need_init = True
+#
+#            i = 0
+#            for wavelength_list in wavelengths:
+#                for wavelength in wavelength_list:
+#                    wavelengths_avg[i] += wavelength
+#                    i += 1
+#
+#            i = 0
+#            for ampl in amplitudes:
+#                for amp in ampl:
+#                    amplitudes_avg[i] += amp
+#                    i += 1
+#
+#            count += 1
+#
+#        i = 0
+#        while i < len(wavelengths_avg):
+#            wavelengths_avg[i] /= (count)
+#            amplitudes_avg[i] /= (count)
+#            i += 1
+#
+#        temp2 = temp_controller.get_temp_c(self.controller)
+#        temperature += float(temp2[:-3])
+#        temperature /= 2.0
+#
+#        serial_nums = ["SN#01", "SN#02", "SN#03", "SN#04", "SN#05", "SN#06", "SN#07",\
+#                        "SN#08", "SN#09", "SN#10", "SN#11", "SN#12"]
+#
+#        self.write_csv_file(self.options.file_name.get(), serial_nums, \
+#                    time.time(), temperature,\
+#                    wavelengths_avg, amplitudes_avg)
 
-        need_init = False
 
-        while count < int(self.num_pts.get()):
-            wavelengths, amplitudes = sm125_wrapper.get_data_actual(self.sm125)
+def write_csv_file(file_name, serial_nums, timestamp, temp, wavelengths, powers):
+    """Write the output csv file."""
+    if os.path.isfile(file_name.get()):
+        file_obj = open(file_name.get(), "a")
+    else:
+        file_obj = open(file_name.get(), "w")
 
-            if not need_init:
-                wavelengths_avg = [0] * len(wavelengths[0])
-                amplitudes_avg = [0] * len(amplitudes[0])
-                need_init = True
+    file_obj.write("Serial Num, Timestamp(s), Temperature (C), "\
+            + "Wavelength (pm), Power (db)\n")
+    file_obj.write("----------------------------------------------"\
+            +"------------------------\n")
 
-            i = 0
-            for wavelength_list in wavelengths:
-                for wavelength in wavelength_list:
-                    wavelengths_avg[i] += wavelength
-                    i += 1
+    i = 0
+    while i < len(serial_nums):
+        file_obj.write(str(serial_nums[i]) + ": " + str(timestamp) + ", " + str(temp) + ", " +\
+                    str(wavelengths[i]) + ", " + str(powers[i]) + "\n")
+        i += 1
 
-            i = 0
-            for ampl in amplitudes:
-                for amp in ampl:
-                    amplitudes_avg[i] += amp
-                    i += 1
-
-            count += 1
-
-        i = 0
-        while i < len(wavelengths_avg):
-            wavelengths_avg[i] /= (count)
-            amplitudes_avg[i] /= (count)
-            i += 1
-
-        temp2 = temp_controller.get_temp_c(self.controller)
-        temperature += float(temp2[:-3])
-        temperature /= 2.0
-
-        serial_nums = ["SN#01", "SN#02", "SN#03", "SN#04", "SN#05", "SN#06", "SN#07",\
-                        "SN#08", "SN#09", "SN#10", "SN#11", "SN#12"]
-
-        self.write_csv_file(serial_nums, time.time(), temperature,\
-                    wavelengths_avg, amplitudes_avg)
-
-
-    def write_csv_file(self, serial_nums, timestamp, temp, wavelengths, powers):
-        """Write the output csv file."""
-        if os.path.isfile(self.file_name.get()):
-            file_obj = open(self.file_name.get(), "a")
-        else:
-            file_obj = open(self.file_name.get(), "w")
-
-        file_obj.write("Serial Num, Timestamp(s), Temperature (C), "\
-                + "Wavelength (pm), Power (db)\n")
-        file_obj.write("----------------------------------------------"\
-                +"------------------------\n")
-
-        i = 0
-        while i < len(serial_nums):
-            file_obj.write(str(serial_nums[i]) + ": " + str(timestamp) + ", " + str(temp) + ", " +\
-                        str(wavelengths[i]) + ", " + str(powers[i]) + "\n")
-            i += 1
-
-        file_obj.write("\n\n")
-        file_obj.close()
+    file_obj.write("\n\n")
+    file_obj.close()
 
 
 
--- create_options_panel.py
+++ create_options_panel.py
@@ -1,145 +1,108 @@
 """Class sets up the tkinter UI code for the options panel."""
 
 import tkinter as tk
-import matplotlib
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
-from numpy import arange, sin, pi
 import ui_helper
-matplotlib.use('TkAgg')
-
-def create_options_grid(app):
-    """Creates the grid for the user to configure options."""
-
-    app_master.title("Kyton Baking")
-    app.menu = tk.Menu(app_master, tearoff=0)
-    app_master.config(menu=app.menu)
-    app.pack(side="top", fill="both", expand=True)
-
-    #Options Grid Init
-    app.options_grid.grid_columnconfigure(1, minsize=50)
-    app.options_grid.grid_columnconfigure(3, minsize=50)
-    row_num = 0
 
 
-    #Instruments Checkboxes
-    app.sm125 = ui_helper.checkbox_entry(app.options_grid,\
-                "Micron Optics SM125", row_num)
-    row_num += 1
+class OptionsPanel(tk.Frame):
+    """Main Tkinter window class."""
+    def __init__(self, master):
+        super().__init__(master)
 
-    app.gp700_state = ui_helper.checkbox_entry(app.options_grid,\
-                "Dicon GP700", row_num)
-    row_num += 1
+        #Init member vars
+        self.sm125_state = tk.IntVar()
+        self.gp700_state = tk.IntVar()
+        self.delta_oven_state = tk.IntVar()
+        self.temp340_state = tk.IntVar()
+        self.sn_ents = []
 
-    app.temp340_state = ui_helper.checkbox_entry(app.options_grid,\
-                "340 Controller", row_num)
-    row_num += 1
-
-    app.delta_oven_state = ui_helper.checkbox_entry(app.options_grid,\
-                "Delta Oven", row_num)
-    row_num += 1
-
-
-    #Number of points to average entry
-    app.num_pts = ui_helper.int_entry(app.options_grid, "Num points to average: ",\
-                row_num, 10, 1)
-    row_num += 1
+        #Init member widgets
+        self.options_grid = tk.Frame(self)
+        self.options_grid.pack()
 
 
-    #Time intervals entry
-    app.prim_time = ui_helper.double_entry(app.options_grid, "Primary time interval: ",\
-                row_num, 10, 1.0)
-    row_num += 1
-    app.sec_time = ui_helper.double_entry(app.options_grid, "Secondary time interval: ",\
-                row_num, 10, 1.0)
-    row_num += 1
+        self.baking_temp = tk.DoubleVar()
+        self.file_name = tk.StringVar()
+        self.sec_time = tk.DoubleVar()
+        self.prim_time = tk.DoubleVar()
+        self.num_pts = tk.IntVar()
+
+        self.create_options_grid()
+        self.start_button = tk.Button(self)
 
 
-    #File name entry
-    app.file_name = ui_helper.string_entry(app.options_grid, "File name: ",\
-                row_num, 15, "kyton_out.csv")
-    row_num += 1
+    def create_options_grid(self):
+        """Creates the grid for the user to configure options."""
+
+        self.pack(side="top", fill="both", expand=True)
+
+        #Options Grid Init
+        self.options_grid.grid_columnconfigure(1, minsize=50)
+        self.options_grid.grid_columnconfigure(3, minsize=50)
+        row_num = 0
 
 
-    #Baking setpoint entry
-    app.baking_temp = ui_helper.double_entry(app.options_grid, "Baking temp: ",\
-                row_num, 10, 250.0)
-    row_num += 1
+        #Instruments Checkboxes
+        self.sm125 = ui_helper.checkbox_entry(self.options_grid,\
+                    "Micron Optics SM125", row_num)
+        row_num += 1
 
+        self.gp700_state = ui_helper.checkbox_entry(self.options_grid,\
+                    "Dicon GP700", row_num)
+        row_num += 1
 
-    #(TEMP) Fiber SN Inputs
-    index = 1
-    while index <= 20:
-        app.sn_ents.append(ui_helper.string_entry(app.options_grid, \
-                "Serial Number " + str(index) + ": ", row_num, 20))
-        index += 1
+        self.temp340_state = ui_helper.checkbox_entry(self.options_grid,\
+                    "340 Controller", row_num)
+        row_num += 1
+
+        self.delta_oven_state = ui_helper.checkbox_entry(self.options_grid,\
+                    "Delta Oven", row_num)
         row_num += 1
 
 
+        #Number of points to average entry
+        self.num_pts = ui_helper.int_entry(self.options_grid, "Num points to average: ",\
+                    row_num, 10, 1)
+        row_num += 1
+
+
+        #Time intervals entry
+        self.prim_time = ui_helper.double_entry(self.options_grid, "Primary time interval: ",\
+                    row_num, 10, 1.0)
+        row_num += 1
+        self.sec_time = ui_helper.double_entry(self.options_grid, "Secondary time interval: ",\
+                    row_num, 10, 1.0)
+        row_num += 1
+
+
+        #File name entry
+        self.file_name = ui_helper.string_entry(self.options_grid, "File name: ",\
+                    row_num, 15, "kyton_out.csv")
+        row_num += 1
+
+
+        #Baking setpoint entry
+        self.baking_temp = ui_helper.double_entry(self.options_grid, "Baking temp: ",\
+                    row_num, 10, 250.0)
+        row_num += 1
+
+
+        #(TEMP) Fiber SN Inputs
+        index = 1
+        while index <= 20:
+            self.sn_ents.append(ui_helper.string_entry(self.options_grid, \
+                    "Serial Number " + str(index) + ": ", row_num, 20))
+            index += 1
+            row_num += 1
+
+    def create_start_btn(self, start):
+        """Creates the start button in the app."""
+        #Start button
+        self.start_button["text"] = "Start"
+        self.start_button["command"] = start
+        self.start_button.pack()
+
+
 if __name__ == "__main__":
-    class Application(tk.Frame):
-        """Main Tkinter window class."""
-        def __init__(self, master):
-            super().__init__(master)
-
-            #Init member vars
-            self.sm125_state = tk.IntVar()
-            self.gp700_state = tk.IntVar()
-            self.delta_oven_state = tk.IntVar()
-            self.temp340_state = tk.IntVar()
-            self.sn_ents = []
-
-            #Init member widgets
-            self.options_grid = tk.Frame(self)
-            self.options_grid.pack()
-
-
-            self.baking_temp = tk.DoubleVar()
-            self.file_name = tk.StringVar()
-            self.sec_time = tk.DoubleVar()
-            self.prim_time = tk.DoubleVar()
-            self.num_pts = tk.IntVar()
-
-
-            #Window setup
-            master.title("Kyton Baking")
-            self.menu = tk.Menu(master, tearoff=0)
-            master.config(menu=self.menu)
-            self.pack(side="top", fill="both", expand=True)
-            create_options_grid(self, master)
-            self.create_start_btn()
-            self.create_graph()
-
-
-        def create_start_btn(self):
-            """Creates the start button in the app."""
-            #Start button
-            self.start_button = tk.Button(self)
-            self.start_button["text"] = "Start"
-            self.start_button["command"] = self.start
-            self.start_button.pack()
-
-        def start(self):
-            ui_helper.print_options(self)
-
-        def create_graph(self):
-            fig = Figure(figsize=(5, 4), dpi=100)
-            sub = fig.add_subplot(111)
-            rng = arange(0.0, 3.0, 0.01)
-            y_vals = sin(2*pi*rng)
-
-            sub.plot(rng, y_vals)
-
-
-            # a tk.DrawingArea
-            canvas = FigureCanvasTkAgg(fig, master=ROOT)
-            canvas.show()
-            canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-            toolbar = NavigationToolbar2TkAgg(canvas, ROOT)
-            toolbar.update()
-            canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-
     ROOT = tk.Tk()
-    Application(master=ROOT).mainloop()
+    OptionsPanel(ROOT).mainloop()
