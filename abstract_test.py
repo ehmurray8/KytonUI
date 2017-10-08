97a8f228ccefb9061646390c8e6af1b4b40ab3f3
ehmurray8 <ehmurray8@gmail.com>
2017-10-08 18:42:38 -0400

    Fixed abstract test.

--- abstract_test.py
+++ abstract_test.py
@@ -2,18 +2,17 @@ class Main:
     def __init__(self):
         self.x = 5
 
-    def test_method():
-        print(self.x)
-        sub_method()
+    def test_method(self):
+        print("Test_method: {}".format(self.x))
+        self.sub_method()
 
 class Sub(Main):
     def __init__(self):
-        #super().__init__()
-        super()
-        super().x += 1
+        super().__init__()
+        self.x += 1
 
-    def sub_method():
-        print(x)
+    def sub_method(self):
+        print("Sub_method: {}".format(self.x))
 
 sub = Sub()
 sub.test_method()
