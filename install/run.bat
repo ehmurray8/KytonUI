139ac478ceb387c638d1a37eeeee4a9feaa1c5db
ehmurray8 <ehmurray8@gmail.com>
2018-02-07 10:48:02 -0500

    Updated batch scripts.

--- install/run.bat
+++ install/run.bat
@@ -1,4 +1,4 @@
-cd C:\Users\phils\Documents\FbgUI
+cd %HOMEPATH%\Documents\FbgUI
 @echo off
 start %HOMEPATH%\AppData\Local\Programs\Python\Python36\pythonw.exe ./main_program.py
 exit
---
+++ install/run_emmetpc.bat
@@ -0,0 +1,4 @@
+cd %HOMEPATH%\source_code\KytonUI
+@echo off
+start %HOMEPATH%\AppData\Local\Programs\Python\Python36\pythonw.exe ./main_program.py --nodev
+exit
