2de6dac2900cb106796659f44574fd95100160b0
ehmurray8 <ehmurray8@gmail.com>
2018-02-07 01:24:59 -0500

    Fixed batch script to be generic.

--- install/run.bat
+++ install/run.bat
@@ -1,4 +1,4 @@
 cd C:\Users\phils\Documents\FbgUI
 @echo off
-start C:\Users\phils\AppData\Local\Programs\Python\Python36\pythonw.exe ./main_program.py
+start %HOMEPATH%\AppData\Local\Programs\Python\Python36\pythonw.exe ./main_program.py
 exit
