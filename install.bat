3e5e2bd5e2c05180e077f558677ad709fd77a193
ehmurray8 <ehmurray8@gmail.com>
2018-04-06 02:18:39 -0400

    Replaced cx_freeze with pyinstaller.

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 C:\Users\Emmet\AppData\Local\Programs\Python\Python35\python.exe -m PyInstaller --clean -n fbgui ^
 --add-data="C:\Users\Emmet\source_code\KytonUI\fbgui\config;.\fbgui" ^
---add-data="C:\Users\Emmet\source_code\KytonUI\fbgui\assets;.\fbgui" -w ^
+--add-data="C:\Users\Emmet\source_code\KytonUI\fbgui\assets;.\fbgui" ^
 "C:\Users\Emmet\source_code\KytonUI\fbgui\main_program.py"
