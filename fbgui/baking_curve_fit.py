84b0375d54ff04a202c27d4f236351fa73476d4d
Emmet Murray <ehmurray8@gmail.com>
2018-10-25 14:08:00 -0400

    v 0.0.8 (#135)

--- fbgui/baking_curve_fit.py
+++ fbgui/baking_curve_fit.py
@@ -1,12 +1,23 @@
+from typing import Iterable, List
+
 import pandas as pd
 import numpy.polynomial.polynomial as poly
 
 
-def curve_fit_baking(data_frame: pd.DataFrame, x_index: int, y_index: int=1):
-    x_column_name = data_frame.columns[x_index]
-    x_values = data_frame[x_column_name]
-
-    y_column_name = data_frame.columns[y_index]
-    y_values = data_frame[y_column_name]
-
+def curve_fit_baking(data_frame: pd.DataFrame, y_index: int, x_index: int=1) -> List[float]:
+    x_values = _get_values(data_frame, x_index)
+    y_values = _get_values(data_frame, y_index)
     return list(reversed(poly.polyfit(x_values, y_values, 1)))
+
+
+def add_baking_trend_line(data_frame: pd.DataFrame, coefficients: Iterable[float], fbg_name: str, x_index: int=1) -> int:
+    x_values = _get_values(data_frame, x_index)
+    y_values = poly.polyval(x_values, coefficients)
+    column_name = "{} Trend Values".format(fbg_name)
+    data_frame[column_name] = y_values
+    return data_frame.columns.tolist().index(column_name)
+
+
+def _get_values(data_frame: pd.DataFrame, index: int) -> Iterable[float]:
+    column_name = data_frame.columns[index]
+    return data_frame[column_name]
